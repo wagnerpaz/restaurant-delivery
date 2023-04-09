a67d1dc9255d5b6d902d2d48884d7019c595fdd7
Wagner Paz <wagner.o.paz@gmail.com>
2023-04-09 09:47:50 -0300

    ignore ts errors on deployment for now

--- components/DraggableGroup.tsx
+++ components/DraggableGroup.tsx
@@ -1,11 +1,13 @@
 import classNames from "classnames";
-import React from "react";
+import React, { ComponentProps } from "react";
 import { DndProvider, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import { ACCEPT } from "./Draggable";
 
-const DraggableGroup = (props) => {
+interface DraggableGroupProps extends ComponentProps<"div"> {}
+
+const DraggableGroup: React.FC<DraggableGroupProps> = (props) => {
   return (
     <DndProvider backend={HTML5Backend}>
       <DraggableGroupContent {...props} />
@@ -13,7 +15,12 @@ const DraggableGroup = (props) => {
   );
 };
 
-const DraggableGroupContent = ({ className, children }) => {
+interface DraggableGroupContentProps extends ComponentProps<"div"> {}
+
+const DraggableGroupContent: React.FC<DraggableGroupContentProps> = ({
+  className,
+  children,
+}) => {
   const [, drop] = useDrop(() => ({
     accept: ACCEPT,
   }));
