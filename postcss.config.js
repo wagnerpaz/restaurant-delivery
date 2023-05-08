402544782739c3d8201020a47a881e3eb1551a42
Wagner Paz <wagner.o.paz@gmail.com>
2023-05-08 10:06:55 -0300

    restore previous postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,19 +3,10 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
     purgecss: {
-      content: [
-        "./pages/**/*.{html,ts,tsx}",
-        "./components/**/*.{html,ts,tsx,jsx}",
-        "./forms/**/*.{html,ts,tsx}",
-        "./modals/**/*.{html,ts,tsx}",
-        "./config/**/*.{html,ts,tsx}",
-        "./node_modules/react-tailwindcss-select/dist/index.esm.js",
-        "./styles/**/*.{css}",
-      ],
-      safelist: ["html", "body"],
+      content: ["./**/*.html"],
     },
-    // ...(process.env.NODE_ENV === "production"
-    //   ? { cssnano: { preset: "default" } }
-    //   : {}),
+    ...(process.env.NODE_ENV === "production"
+      ? { cssnano: { preset: "default" } }
+      : {}),
   },
 };
