f442abf8ba2c7ca2f21baa54873ee9e63f65d5c8
Wagner Paz <wagner.o.paz@gmail.com>
2023-05-08 10:11:12 -0300

    now it's correct

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    purgecss: {
-      content: ["./**/*.html"],
-    },
     ...(process.env.NODE_ENV === "production"
       ? { cssnano: { preset: "default" } }
       : {}),
